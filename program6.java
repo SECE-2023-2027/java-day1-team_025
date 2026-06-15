62f192e75c63bb7c92349952921db882d6083504
Kishore <kishorekumar7373g@gmail.com>
2026-06-15 21:02:55 +0530

    prgm6

---
+++ program6.java
@@ -0,0 +1,9 @@
+public class Program6 {
+    public static void main(String[] args) {
+        int n = 15;
+        String hex = Integer.toHexString(n).toUpperCase();
+
+        System.out.println("Input a decimal number: " + n);
+        System.out.println("Hexadecimal number is : " + hex);
+    }
+}
