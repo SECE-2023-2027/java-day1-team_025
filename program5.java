f4bf877abfa65019491328e8f97517916abc779f
Kishore <kishorekumar7373g@gmail.com>
2026-06-15 21:01:10 +0530

    prgm5

---
+++ program5.java
@@ -0,0 +1,8 @@
+public class Program5 {
+    public static void main(String[] args) {
+        int x=5;
+        String y=Integer.toBinaryString(x);
+        System.out.println("Input a Decimal Number : "+x);
+        System.out.println("Binary number is: "+y);
+    }
+}
