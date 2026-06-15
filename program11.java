cc120e0963ba6702fffdba9460d2c8f47ec2e0d1
Kishore <kishorekumar7373g@gmail.com>
2026-06-15 21:19:38 +0530

    prgm11

---
+++ program11.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class problem11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input a string: ");
+        String S = sc.nextLine();
+        int letters = 0;
+        int spaces = 0;
+        int numbers = 0;
+        for (int i = 0; i < S.length(); i++) {
+            char ch = S.charAt(i);
+
+            if (Character.isLetter(ch)) {
+                letters++;
+            } else if (Character.isDigit(ch)) {
+                numbers++;
+            } else if (Character.isSpaceChar(ch)) {
+                spaces++;
+            }
+        }
+
+        System.out.println("The string is : " + text);
+        System.out.println("letter: " + letters);
+        System.out.println("space: " + spaces);
+        System.out.println("number: " + numbers);
+        sc.close();
+    }
+}
