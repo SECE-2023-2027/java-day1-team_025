230b4303894306eed6c7f0a3df81feab9e2638fd
Kishore <kishorekumar7373g@gmail.com>
2026-06-15 20:56:21 +0530

    prgm3

---
+++ program3.java
@@ -0,0 +1,10 @@
+public class Program3 {
+    public static void main(String[] args) {
+        double r = 7.5;
+        double area = Math.PI * r * r;
+        double perimeter = 2 * Math.PI * r;
+        System.out.println("Radius: " + r);
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+    }
+}
